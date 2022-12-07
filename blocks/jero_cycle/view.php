6fbcc5d56a4e1addc067510757986512bc6a3a7a
Jeremy Rogers <info@jero.co.nz>
2022-12-07 17:50:15 +1300

    Footer and containers

--- blocks/jero_cycle/view.php
+++ blocks/jero_cycle/view.php
@@ -41,7 +41,7 @@ if ($effect == 'continuous') {
 	data-cycle-max-z="<?php  echo $maxZ?>"
 
 	 data-cycle-overlay-template='<div class="cycle-caption" style="z-index:<?php  echo $maxZ ? $maxZ +10 : 110?>">{{h2link}}<h4>{{desc}}</h4><span class="<?php  echo $buttonCSS ? $buttonCSS : 'btn btn-default' ?>"><a class="{{hiddenclass}}" href="{{link}}">{{buttontext}}</a></span></div>'
-	 data-cycle-auto-height="<?php  echo $ratio ?>">
+	 data-cycle-auto-height="calc">
 	<div class="cycle-overlay"></div>
 	<?php 
 	switch ($navigationType) {
